704a5487ef72aa8efea4c9f4620bf6679d117594
Shreerang Vaidya <smv@pop-os.localdomain>
2022-08-17 15:12:29 +0530

    very basic server

--- server.c
+++ server.c
@@ -1,32 +1,33 @@
-#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+// needed for server stuff
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
 
+#define CLIENT_NUM (1)  // 1 client
+
 int main() {
-  // meet sock, the socket
-  int sock = socket(AF_INET, SOCK_STREAM, 0);
-  // this is the address
+  char message[256] = "Hello, world.";
+  // meet server sock, the server socket
+  int server_sock = socket(AF_INET, SOCK_STREAM, 0);
+  // server address
   struct sockaddr_in addr = {
       .sin_family = AF_INET,
+      .sin_addr.s_addr = inet_addr("127.0.0.1"),
       .sin_port = htons(3000),
-      .sin_addr.s_addr = INADDR_ANY,
   };
-  // connect and get the status back
-  int status = connect(sock, (struct sockaddr *)&addr, sizeof(addr));
-  if (status == -1) {
-    printf("Connection error!\n");  // oof
-    exit(EXIT_FAILURE);
-  }
-  char response[256];  // just a big char array to get response
+  // woo BIND to port and stuff
+  bind(server_sock, (struct sockaddr *)&addr, sizeof(addr));
+  listen(server_sock, CLIENT_NUM);
+  // client sock is a free elf
+  int client_sock = accept(server_sock, NULL, NULL);
+  // then send
+  send(client_sock, message, sizeof(message), 0);  // yay
+  close(server_sock);
 
-  // "recieve"
-  // no flags because I have 0 knowledge about this
-  recv(sock, &response, sizeof(response), 0);
-  printf("Data: %s\n", response);
-  close(sock);
   return 0;
 }
 
